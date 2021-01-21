240dad6418e2b9c474f1999033bfa129daa2c5a7
Oscar Andersson <oscar.andersson.200@gmail.com>
2021-01-21 14:10:36 +0100

    adds docker compose and docker compose command line completion to setup_docker.sh.

--- scripts/setup_docker.sh
+++ scripts/setup_docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# DOCKER
 # Install
 curl -fsSL "https://get.docker.com" -o /tmp/get-docker.sh
 sudo sh /tmp/get-docker.sh
@@ -12,5 +13,12 @@ sudo usermod -aG docker $USER
 sudo systemctl enable docker
 sudo systemctl start docker
 
-# Done
-docker --version
+# DOCKER COMPOSE
+# Install
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# DOCKER COMPOSE COMMAND LINE COMPLETION
+# Install
+sudo curl -L "https://raw.githubusercontent.com/docker/compose/1.28.0/contrib/completion/bash/docker-compose" -o /etc/bash_completion.d/docker-compose
