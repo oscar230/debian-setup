9f408a4c0d6b93bf66f42059a64a5a72f1c1ccbe
Oscar Andersson <oscar.andersson.200@gmail.com>
2021-01-26 12:20:05 +0100

    add duckduckgo as homepage for chromium via Tor.

--- scripts/setup_chromium.sh
+++ scripts/setup_chromium.sh
@@ -10,7 +10,7 @@ Version = 1.0
 Type = Application
 Terminal = false
 Name=Tor Chromium Web Browser
-Exec =chromium --proxy-server=socks5://localhost:9050 --incognito --temp-profile
+Exec =chromium --proxy-server=socks5://localhost:9050 --incognito --temp-profile https://duckduckgo.com/
 Icon=chromium
 Categories=Network;WebBrowser;
 " > /tmp/chromium_tor.desktop
